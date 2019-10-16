138f5cd128d8dabcd59e3c58fd8018a730233b1f
josephlaotzu <josephlao.1398@gmail.com>
2019-10-16 03:53:01 -0400

    add tablet and desktop view

--- js/main.js
+++ js/main.js
@@ -1,6 +1,23 @@
 // toggle script for navigation menu
+let toggledMenu = false;
 function toggleMenu() {
+    toggledMenu = !toggledMenu;
     document.getElementsByClassName('menu-nav')[0].classList.toggle('expanded');
-    document.getElementsByClassName('cl-icon humberger')[0].classList.toggle('close');
+    const close = document.getElementsByClassName('cl-icon humberger')[0].classList;
+    if (toggledMenu) {
+        document.getElementsByClassName('menu-list site')[0].classList.add('show');
+        document.getElementsByClassName('menu-list account')[0].classList.remove('show');
+        close.add('close');
+    } else {
+        close.remove('close');
+    }
 
 }
+
+function toggleUser() {
+    document.getElementsByClassName('menu-nav')[0].classList.toggle('expanded');
+    document.getElementsByClassName('cl-icon humberger')[0].classList.remove('close');
+    document.getElementsByClassName('menu-list account')[0].classList.add('show');
+    document.getElementsByClassName('menu-list site')[0].classList.remove('show');
+}
+
