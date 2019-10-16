1c551564e9fd333aa69114509c69021b6566edb7
Joseph Lao <n01370825@nj-00118-017.humber.org>
2019-10-15 21:08:20 -0400

    init commit

---
+++ js/main.js
@@ -0,0 +1,6 @@
+// toggle script for navigation menu
+function toggleMenu() {
+    document.getElementsByClassName('menu-nav')[0].classList.toggle('expanded');
+    document.getElementsByClassName('cl-icon humberger')[0].classList.toggle('close');
+
+}
